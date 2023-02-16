07801c41530931dc313b06b697b4adb6e952c4f2
Ryan de Menezes <menezesryan1010@gmail.com>
2023-02-16 09:48:31 -0300

    User Seeder in Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            BalanceSeeder::class,
-            HistoricSeeder::class,
+            // BalanceSeeder::class,
+            // HistoricSeeder::class,
         ]);
     }
 }
